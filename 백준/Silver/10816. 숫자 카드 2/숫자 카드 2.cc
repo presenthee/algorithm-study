85ca27cdb1d22a84de6102e2d4c4b23fe3ea9ec1
presenthee <77828537+presenthee@users.noreply.github.com>
2023-08-10 20:21:24 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 444 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -3,9 +3,48 @@
 
 using namespace std;
 
-int LowerBound(vector<int>& cards, int target) {}
+int LowerBound(vector<int>& cards, int target) {
+  int low = 0;
+  int high = cards.size() - 1;
 
-int UpperBound(vector<int>& cards, int target) {}
+  if (cards[high] < target) {
+    return high + 1;
+  } else if (cards[low] >= target) {
+    return 0;
+  }
+
+  while (low + 1 < high) {
+    int mid = (low + high) / 2;
+    if (cards[mid] >= target) {
+      high = mid;
+    } else {
+      low = mid;
+    }
+  }
+  return high;
+}
+
+int UpperBound(vector<int>& cards, int target) {
+  int low = 0;
+  int high = cards.size() - 1;
+
+  if (cards[high] <= target) {
+    return high + 1;
+  } else if (cards[low] > target) {
+    return 0;
+  }
+
+  while (low + 1 < high) {
+    int mid = (low + high) / 2;
+    if (cards[mid] > target) {
+      high = mid;
+    } else {
+      low = mid;
+    }
+  }
+
+  return high;
+}
 
 int main() {
   int N, M;
@@ -19,8 +58,7 @@ int main() {
   for (int i = 0; i < M; i++) {
     int target;
     scanf("%d", &target);
-    int num_target = upper_bound(cards.begin(), cards.end(), target) -
-                     lower_bound(cards.begin(), cards.end(), target);
+    int num_target = UpperBound(cards, target) - LowerBound(cards, target);
     printf("%d ", num_target);
   }
 
